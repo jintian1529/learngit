24e008f29a4e9899fcb396985a660dba6d140039
fdj <wx-fudj@dtdream.com>
2018-07-27 20:08:15 +0800

    spring cloud

---
+++ SpringCloud/eurekaserver01/src/main/java/com/example/eurekaserver01/Eurekaserver01Application.java
@@ -0,0 +1,14 @@
+package com.example.eurekaserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Eurekaserver01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eurekaserver01Application.class, args);
+    }
+}
